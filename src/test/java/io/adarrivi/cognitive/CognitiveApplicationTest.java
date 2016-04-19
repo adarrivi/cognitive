cfcb61c95f610bb47e6720f130c3c08f5b3f483f
Alvaro de Miguel <adarrivi@gmail.com>
2016-04-19 21:46:29 +0200

    First commit with the skeleton of the project

---
+++ src/test/java/io/adarrivi/cognitive/CognitiveApplicationTest.java
@@ -0,0 +1,15 @@
+package io.adarrivi.cognitive;
+
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class CognitiveApplicationTest {
+
+    @Test
+    public void sampleTest() {
+        assertThat(true, is(true));
+    }
+
+}

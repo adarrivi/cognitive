2e2afb9236c8c338c24622c4eea11480edba4067
Alvaro de Miguel <adarrivi@gmail.com>
2016-06-05 20:50:02 +0200

    Trying approach with multiple observables

--- src/test/java/io/adarrivi/cognitive/CognitiveApplicationTest.java
+++ src/test/java/io/adarrivi/cognitive/CognitiveApplicationTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.Observable;
+import rx.functions.Action1;
+import rx.observables.ConnectableObservable;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -27,8 +29,42 @@ public class CognitiveApplicationTest {
         })
                 .sample(1, TimeUnit.SECONDS)
                 .subscribe(i -> logger.info("{}", i));
+        sleepMainThread();
+    }
+
+    int i;
+
+    @Test
+    public void duplicateObservable() {
+
+        Action1<Integer> subscriberA = value -> logger.info("Subscribe A {} {}", value, i++);
+        Action1<Integer> subscriberB = value -> logger.info("Subscribe B {} {}", value, i++);
+
+        Observable<Integer> observable = Observable.just(1, 2, 3)
+                .map(value -> value * 2);
+        observable
+                .map(value -> value * 2)
+                .subscribe(subscriberA);
+        observable.subscribe(subscriberB);
+
+        i = 0;
+        ConnectableObservable<Integer> observable1 = Observable.just(1, 2, 3)
+                .map(value -> value * 2)
+                .publish();
+        observable1
+                .map(value -> value * 2)
+                .subscribe(subscriberA);
+        observable1.subscribe(subscriberB);
+        observable1.connect();
+    }
+
+    private void sleepMainThread() {
         logger.info("Sleeping main thread");
-        Thread.sleep(10000);
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
